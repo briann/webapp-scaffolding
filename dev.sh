86855745a38b059d506927acb03fe7f1fc664924
Brian Ngo <brian@brianngo.net>
2017-03-11 18:24:19 -0800

    Remove dev.sh - was not used

--- dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=dev-password -d mysql:5.7
-docker run --name dev-env --link mysql:mysql -p 8080:8080 --volume $(pwd):/opt/dev -it briann/dev-env:latest /bin/bash
