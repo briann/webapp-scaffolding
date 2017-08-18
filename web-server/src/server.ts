d09bf8f02bc9b7bc7b23fd850b9088a1a9f4fbfd
Brian Ngo <brian@brianngo.net>
2017-08-18 15:06:19 -0700

    Add body parsing

--- web-server/src/server.ts
+++ web-server/src/server.ts
@@ -1,3 +1,4 @@
+import * as bodyparser from "body-parser";
 import * as compression from "compression";
 import * as config from "config";
 import * as express from "express";
@@ -8,6 +9,8 @@ const server = express();
 server.set("view engine", "pug");
 server.set("views", "./web-server/templates");
 server.use(compression());
+server.use(bodyparser.json());
+server.use(bodyparser.urlencoded({extended: true}));
 const bundleBaseUrl = configureClientBundle(server);
 
 // tslint:disable-next-line:variable-name
@@ -19,6 +22,5 @@ server.get("/", (_request: express.Request, response: express.Response) => {
 
 const port = config.get("server.port");
 server.listen(port, () => {
-    // tslint:disable-next-line:no-console
     console.log(`Started server on port ${port}`);
 });
